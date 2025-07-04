dd2fb289bbc2c5a7ee2390d8dbc9860465568464
Pratham Tamrakar <prathamtamrakar849@gmail.com>
2025-07-04 12:40:49 +0530

    Redux Toolkit Added

---
+++ 13_reduxToolkit/src/App.jsx
@@ -0,0 +1,15 @@
+import AddTodo from "./components/AddTodo"
+import Todo from "./components/Todo"
+
+
+function App() {
+
+  return (
+    <div className="px-48">
+      <AddTodo/>
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
